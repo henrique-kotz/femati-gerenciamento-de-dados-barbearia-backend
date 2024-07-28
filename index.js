fbf51d265f638c2f0cee40d2d9c8467d7ee2466c
Henrique Kötz <henriquebkotz@gmail.com>
2024-07-28 20:54:15 -0300

    find and insert document

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import { MongoClient } from "mongodb";
+import express from "express";
+import cors from "cors";
 
 const uri = "mongodb+srv://henriquebkotz:bcUrLHd4gDSh8GyW@barbearia-db.ihxjsbx.mongodb.net/?retryWrites=true&w=majority&appName=barbearia-db";
 
@@ -18,4 +20,53 @@ async function run() {
     }
 }
 
-run().catch(console.dir);
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+    try {
+        const database = client.db('barbearia_db');
+        const customers = database.collection('barbearia_tb');
+
+        const cursor = customers.find();
+
+        const data = [];
+        for await (const doc of cursor) {
+            data.push(doc);
+        }
+
+        res.send(data);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+    finally {
+        await client.close();
+    }
+})
+
+app.post('/', async (req, res) => {
+    try {
+        const { body } = req;
+
+        const database = client.db('barbearia_db');
+        const customers = database.collection('barbearia_tb');
+
+        await customers.insertOne(body);
+
+        res.sendStatus(201);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    } finally {
+        await client.close();
+    }
+})
+
+
+app.listen(5000, () => {
+    console.log('Listening on port 5000...');
+})
+
+//run().catch(console.dir);
